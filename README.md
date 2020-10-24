15402331f108028b9aa07e655045cf44daff7ae2
QG <Quartergeekster@live.co.uk>
2020-10-24 18:49:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# No More Nomads
+
+## Introduction
+This is one of my first forays into game development, and is a joint project building a platform puzzle game inspired by Lemmings. The end goal is to produce a lightweight item-based puzzle game with a wide variety of levels
+
+##Basic information
+* This is planned to be a 2D sidescrolling puzzle platformer built in Unity 2019.4.11f1
+* All code is currently written in C#.
+* The repo includes all images and animations used for the project
+
+## Current State
+* The game is in early design and PoC stage, with intention to develop it forward into something of note. 
+* Currently all image assets are sourced from Kenney Vleugels (www.kenney.nl). This is a temporary measure, but it's amazingly nice of them to grant use for personal and commercial projects. 
+* As this is early stages, almost all functionality is likely to change.
---
+++ TileMap Test 1/Assets/Scripts/Controllers/NomadController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Nomads.Entities.Controllers{
+    public class NomadController : BaseEntityController
+    {
+        [Header("Nomad Abilities")]
+        [SerializeField] public bool canDestroy;
+
+        [HideInInspector] public const float DestructionRangeRadius = 0.1f;
+        public override void Start()
+        {
+            base.Start();
+        }
+
+
+        public override void FixedUpdate() 
+        {
+            base.FixedUpdate();
+            if (isAwake)
+            {
+                if (isGrounded || AirControl) //Can player move
+                {
+                    if (FacingRight) //Move Right
+                    {
+                        Vector3 targetVelocity = new Vector2(MoveSpeed * 10f, entity_RigidBody.velocity.y);
+                        entity_RigidBody.velocity = Vector2.SmoothDamp(entity_RigidBody.velocity, targetVelocity, ref CurrentVelocity, 1 / AccelerationSpeed);
+                    }
+                    else //Move Left
+                    {
+                        Vector3 targetVelocity = new Vector2(-MoveSpeed * 10f, entity_RigidBody.velocity.y);
+                        entity_RigidBody.velocity = Vector2.SmoothDamp(entity_RigidBody.velocity, targetVelocity, ref CurrentVelocity, 1 / AccelerationSpeed);
+                    }
+                }
+
+                if(isGrounded)
+                {
+                    //if(FacingRight)
+                    //{
+                    //    Collider2D[] colliders = Physics2D.OverlapCircleAll(RightTransform.position, GroundCheckRadius);
+                    //    for (int i = 0; i < colliders.Length; i++)
+                    //    {
+                    //        if (colliders[i].gameObject != gameObject)
+                    //        {
+                    //            //Debug.Log("Right Collision Detected");
+                    //        }
+                    //    }
+                    //}
+                }
+            }
+        }
+
+        void OnCollisionEnter2D(Collision2D Coll)
+        {
+            if(Coll.otherCollider == RightCollider)
+            {
+                if(Coll.collider.gameObject.name == "Terrain")
+                {
+                    Debug.Log("Collision detected");
+                    for(int i = 0; i < Coll.contacts.Length; i++)
+                    {
+                        var position = Coll.contacts[i].point;
+                        Coll.collider.gameObject.GetComponent<Tilemap>().SetTile(Coll.gameObject.GetComponent<Tilemap>().WorldToCell(position), null);
+                    }
+                }
+            }
+        }
+    }
+}
