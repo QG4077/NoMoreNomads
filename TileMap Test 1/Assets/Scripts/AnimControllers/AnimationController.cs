aac3b028e41f547b165a3656c262726d74cf69e8
paxwort <paxwort@gmail.com>
2020-11-28 18:43:24 +0000

    Added Animated Nomad Sprites

---
+++ TileMap Test 1/Assets/Scripts/AnimControllers/AnimationController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rigid2DAnimationController : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody2D rb2D;
+
+    public virtual void Start()
+    {
+        if (anim == null) { anim = this.GetComponent<Animator>(); }
+        if (rb2D == null) { rb2D = this.GetComponent<Rigidbody2D>(); }
+    }
+}
